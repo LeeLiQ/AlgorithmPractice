d8278c0560f31b1a79d8ea39219921de2b599d11
Qian Li <lqustc@gmail.com>
2018-04-15 17:14:34 -0700

    reverse a string or words that are separated by space in place.

---
+++ pure-junk/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pure_junk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ reverse-word-in-place/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace reverse_word_in_place
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = "one two three";
+            var charArray = input.ToCharArray();
+            ReverseChars(charArray, 0, input.Length - 1);
+            int start = 0, end = 0;
+            while (start < input.Length)
+            {
+                if (charArray[start] == ' ')
+                {
+                    start++;
+                    end++;
+                }
+                else if (end == input.Length || charArray[end] == ' ')
+                {
+                    ReverseChars(charArray, start, end - 1);
+                    start = end + 1;
+                    end++;
+                }
+                else
+                    end++;
+            }
+            System.Console.WriteLine(new string(charArray));
+            System.Console.WriteLine("++++++++++++++++++++");
+        }
+
+        private static void ReverseChars(char[] arr, int start, int end)
+        {
+            while (start < end)
+            {
+                var temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                start++;
+                end--;
+            }
+        }
+    }
+}
