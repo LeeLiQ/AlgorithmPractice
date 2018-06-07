18527f561add7ba731a66ebed2395a36bcb3d790
Qian Li <lqustc@gmail.com>
2018-06-06 21:30:35 -0700

    reverse linked list in K group

---
+++ reverse-linked-list-k-group/ListNode.cs
@@ -0,0 +1,14 @@
+namespace reverse_linked_list_k_group
+{
+    public class ListNode
+    {
+        public int val { get; set; }
+
+        public ListNode next { get; set; }
+
+        public ListNode(int x)
+        {
+            val = x;
+        }
+    }
+}
---
+++ reverse-linked-list-k-group/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace reverse_linked_list_k_group
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var head = new ListNode(1);
+            head.next = new ListNode(2);
+            head.next.next = new ListNode(3);
+            head.next.next.next = new ListNode(4);
+            head.next.next.next.next = new ListNode(5);
+            var result = ReverseKGroup(head, 5);
+
+            while (result != null)
+            {
+                System.Console.Write(result.val + "-->");
+                result = result.next;
+            }
+        }
+
+        private static ListNode ReverseKGroup(ListNode head, int k)
+        {
+            var node = head;
+            var count = 0;
+
+            while (count < k)
+            {
+                if (node == null)
+                    return head;
+                node = node.next;
+                count++;
+            }
+
+            var pre = ReverseKGroup(node, k);
+            while (count > 0)
+            {
+                var next = head.next;
+                head.next = pre;
+                pre = head;
+                head = next;
+                count--;
+            }
+            return pre;
+        }
+    }
+}
