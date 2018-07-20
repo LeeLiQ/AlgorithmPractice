7da200466dc30fe88dd81593d39fedefd5362b76
Qian Li <lqustc@gmail.com>
2018-07-19 23:15:26 -0700

    recursive reverse string

---
+++ reverse-string-recursive/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace reverse_string_recursive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = "12345";
+
+            Console.WriteLine(Reverse(input));
+        }
+
+        private static string Reverse(string input)
+        {
+            if (input.Length == 1)
+                return input;
+
+            return Reverse(input.Substring(1)) + input[0];
+        }
+    }
+}
