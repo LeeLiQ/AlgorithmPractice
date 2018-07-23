e88f77b1ade75861b0d5283b860a8154ef836424
Qian Li <lqustc@gmail.com>
2018-07-23 00:57:07 -0700

    sort colors using three pointers left, middle, right. Move middle and then left right accordingly.

---
+++ k-diff-pair-in-array/remove-duplicate-from-array-k-distance/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace remove_duplicate_from_array_k_distance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = new int[] { 1, 1, 1, 2, 2, 3 };
+            RemoveDuplicate(nums);
+            foreach (var num in nums)
+                System.Console.WriteLine(num);
+        }
+
+        private static int RemoveDuplicate(int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+                return 0;
+
+            int i = 0, j = 0;
+            while (j < nums.Length)
+            {
+                if (i < 2)
+                    i++;
+                else
+                {
+                    if (nums[j] > nums[i - 2])
+                        nums[i++] = nums[j];
+                }
+                j++;
+            }
+            return i;
+        }
+    }
+}
---
+++ sort-colors/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace sort_colors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = new int[] { 2, 0, 2, 1, 1, 0 };
+            SortColors(input);
+        }
+
+        private static void SortColors(int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+                return;
+            int left = 0, right = nums.Length - 1, middle = 0;
+
+            while (middle <= right)
+            {
+                if (nums[middle] == 1)
+                    middle++;
+                else if (nums[middle] == 2)
+                {
+                    var temp2 = nums[middle];
+                    nums[middle] = nums[right];
+                    nums[right] = temp2;
+                    right--;
+                }
+                else
+                {
+                    var temp0 = nums[middle];
+                    nums[middle] = nums[left];
+                    nums[left] = temp0;
+                    middle++;
+                    left++;
+                }
+            }
+        }
+    }
+}
